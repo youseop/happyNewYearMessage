7ec6ba98faa06641d034dce10fd0a3ae601aa6b2
youseop <edlsw@naver.com>
2021-12-27 18:48:19 +0900

    delete font.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./style.css";
-import "./static/fonts/font.css";
 
 ReactDOM.render(
   <React.StrictMode>
