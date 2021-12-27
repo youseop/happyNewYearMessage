60361a18cfb2c980b47951971faf73fc7de79ed0
youseop <youseop.lee@voyagerx.com>
2021-12-27 18:54:42 +0900

    Updates

---
+++ precache-manifest.73770562d9d72bcdc79c81586944b1a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b84d14405e8ba9882593",
+    "url": "/happyNewYearMessage/static/css/main.1835c12d.chunk.css"
+  },
+  {
+    "revision": "b84d14405e8ba9882593",
+    "url": "/happyNewYearMessage/static/js/main.53e02ebe.chunk.js"
+  },
+  {
+    "revision": "ceec8df3b53e0cae1644",
+    "url": "/happyNewYearMessage/static/js/runtime~main.38b566d7.js"
+  },
+  {
+    "revision": "6c76894cd67657c0bb0e",
+    "url": "/happyNewYearMessage/static/js/2.44e374fa.chunk.js"
+  },
+  {
+    "revision": "fc54496dc9ede22506af54e670e1b4b1",
+    "url": "/happyNewYearMessage/static/media/YdestreetL.fc54496d.ttf"
+  },
+  {
+    "revision": "4fadbb68887dbb41657fd2b9986c6563",
+    "url": "/happyNewYearMessage/static/media/YdestreetB.4fadbb68.ttf"
+  },
+  {
+    "revision": "0a9eca91f6dd34f0ac931c29bf512137",
+    "url": "/happyNewYearMessage/static/media/DOSMyungjo.0a9eca91.ttf"
+  },
+  {
+    "revision": "227d33da747fd1d26d73b03e0b325300",
+    "url": "/happyNewYearMessage/index.html"
+  }
+];
