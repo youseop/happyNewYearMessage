7bc6a6a9eab0bdb363f0cd1c7f09de5a4a972f77
youseop <youseop.lee@voyagerx.com>
2021-12-28 00:33:52 +0900

    Updates

---
+++ precache-manifest.df3eafc4228ad5a6bababf65b57b83e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f60f215b29ced3d79b92",
+    "url": "2022message.com/static/css/main.2471d7b3.chunk.css"
+  },
+  {
+    "revision": "f60f215b29ced3d79b92",
+    "url": "2022message.com/static/js/main.ed6e408b.chunk.js"
+  },
+  {
+    "revision": "95c8e71d5775efcfbd46",
+    "url": "2022message.com/static/js/runtime~main.27bb5798.js"
+  },
+  {
+    "revision": "46d6adf817b9b8487085",
+    "url": "2022message.com/static/js/2.578cdc79.chunk.js"
+  },
+  {
+    "revision": "fc54496dc9ede22506af54e670e1b4b1",
+    "url": "2022message.com/static/media/YdestreetL.fc54496d.ttf"
+  },
+  {
+    "revision": "4fadbb68887dbb41657fd2b9986c6563",
+    "url": "2022message.com/static/media/YdestreetB.4fadbb68.ttf"
+  },
+  {
+    "revision": "0a9eca91f6dd34f0ac931c29bf512137",
+    "url": "2022message.com/static/media/DOSMyungjo.0a9eca91.ttf"
+  },
+  {
+    "revision": "37c1663a4c3216589e5c93fbc77a21d1",
+    "url": "2022message.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/happyNewYearMessage/precache-manifest.7c1633b9d471f3be702c7f5596c391a2.js"
+  "2022message.com/precache-manifest.df3eafc4228ad5a6bababf65b57b83e4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/happyNewYearMessage/index.html", {
+workbox.routing.registerNavigationRoute("2022message.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
