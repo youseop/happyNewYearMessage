0109ff7eab0e7843ae594f8722b943d0229faaf9
youseop <edlsw@naver.com>
2021-12-28 03:29:11 +0900

    Updates

---
+++ precache-manifest.e99102d9a72b0d077c99482e3c977371.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2331071bcdeb5adfdeac",
+    "url": "/static/css/main.9d46d426.chunk.css"
+  },
+  {
+    "revision": "2331071bcdeb5adfdeac",
+    "url": "/static/js/main.86f92203.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a155cdfa570ea0e7a4d",
+    "url": "/static/js/2.3e8afc38.chunk.js"
+  },
+  {
+    "revision": "fc54496dc9ede22506af54e670e1b4b1",
+    "url": "/static/media/YdestreetL.fc54496d.ttf"
+  },
+  {
+    "revision": "4fadbb68887dbb41657fd2b9986c6563",
+    "url": "/static/media/YdestreetB.4fadbb68.ttf"
+  },
+  {
+    "revision": "0a9eca91f6dd34f0ac931c29bf512137",
+    "url": "/static/media/DOSMyungjo.0a9eca91.ttf"
+  },
+  {
+    "revision": "8eac67b88dce3778d0968b400700c161",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "2022message.com/precache-manifest.dbdbf1d5d62e5da23c953584bba240d6.js"
+  "/precache-manifest.e99102d9a72b0d077c99482e3c977371.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("2022message.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
